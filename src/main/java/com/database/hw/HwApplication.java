a311c889aebcb1764e1fd4f18563241ffbccd9c1
靳炳淑 <jinbingshu_nju@qq.com>
2018-10-30 20:32:24 +0800

    注释

--- src/main/java/com/database/hw/HwApplication.java
+++ src/main/java/com/database/hw/HwApplication.java
@@ -51,11 +51,19 @@ public class HwApplication {
         searchThisMonthCost(userService);
     }
 
+    /**
+     * 添加用户
+     * @param companyService
+     */
     private static void addUser(CompanyService companyService){
         companyService.addUser(1,"qyc","15952000000");
         companyService.addUser(2,"jbs","15952022680");
     }
 
+    /**
+     * 公司制定通话套餐
+     * @param companyService
+     */
     private static void addCallPackage(CompanyService companyService){
         companyService.addCallPackage("通话套餐1",20.0,100.0);
         companyService.addCallPackage("通话套餐2",30.0,150.0);
@@ -63,23 +71,39 @@ public class HwApplication {
         companyService.addCallPackage("通话套餐4",50.0,300.0);
     }
 
+    /**
+     * 公司制定短信套餐
+     * @param companyService
+     */
     private static void addMessagePackage(CompanyService companyService){
         companyService.addMessagePackage("短信套餐1",10.0,200);
         companyService.addMessagePackage("短信套餐2",15.0,400);
     }
 
+    /**
+     * 公司制定本地流量套餐
+     * @param companyService
+     */
     private static void addLocalSurfPackage(CompanyService companyService){
         companyService.addLocalSurfPackage("本地流量套餐1",20.0,2048);
         companyService.addLocalSurfPackage("本地流量套餐2",25.0,4096);
         companyService.addLocalSurfPackage("本地流量套餐3",30.0,8192);
     }
 
+    /**
+     * 公司制定国内流量套餐
+     * @param companyService
+     */
     private static void addDomesticSurfPackage(CompanyService companyService){
         companyService.addDomesticSurfPackage("国内流量套餐1",30.0,2048);
         companyService.addDomesticSurfPackage("国内流量套餐2",35.0,4096);
         companyService.addDomesticSurfPackage("国内流量套餐3",40.0,8192);
     }
 
+    /**
+     * 用户订购通话套餐
+     * @param userService
+     */
     private static void userAddCallPackage(UserService userService){
         double t1 = System.currentTimeMillis();
 //        String res = userService.userAddPackage(1,1,"通话套餐1","20180830000000");
@@ -89,6 +113,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户订购短信套餐
+     * @param userService
+     */
     private static void userAddMessagePackage(UserService userService){
         double t1 = System.currentTimeMillis();
 //        String res = userService.userAddPackage(1,2,"短信套餐1","20180830000000");
@@ -98,6 +126,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户订购本地流量套餐
+     * @param userService
+     */
     private static void userAddLocalPackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userAddPackage(1,3,"本地流量套餐2","20180831000000");
@@ -106,6 +138,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户订购国内流量套餐
+     * @param userService
+     */
     private static void userAddDomesticPackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userAddPackage(1,4,"国内流量套餐2","20180830000000");
@@ -114,6 +150,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户退订通话套餐
+     * @param userService
+     */
     private static void userRemoveCallPackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userRemovePackage(1,1,"电话套餐1","20180829000000");
@@ -122,6 +162,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户退订短信套餐
+     * @param userService
+     */
     private static void userRemoveMessagePackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userRemovePackage(1,2,"短信套餐1","20180829000000");
@@ -130,6 +174,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户退订本地流量套餐
+     * @param userService
+     */
     private static void userRemoveLocalPackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userRemovePackage(1,3,"本地流量套餐1","20180829000000");
@@ -138,6 +186,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户退订国内流量套餐
+     * @param userService
+     */
     private static void userRemoveDomesticPackage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res = userService.userRemovePackage(1,4,"国内流量套餐1","20180829000000");
@@ -146,6 +198,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户产生通话时长
+     * @param userService
+     */
     private static void userDoCall(UserService userService){
         double t1 = System.currentTimeMillis();
         String res =userService.doCall(1,"20180916202000",12);
@@ -154,6 +210,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户发短信
+     * @param userService
+     */
     private static void userDoMessage(UserService userService){
         double t1 = System.currentTimeMillis();
         String res =userService.sendMessage(1,"20180923000000");
@@ -162,6 +222,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户产生本地流量
+     * @param userService
+     */
     private static void userDoLocal(UserService userService){
         double t1 = System.currentTimeMillis();
         String res =userService.localSurf(1,"20180922000000",50);
@@ -170,6 +234,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户产生国内流量
+     * @param userService
+     */
     private static void userDoDomestic(UserService userService){
         double t1 = System.currentTimeMillis();
         String res =userService.domesticSurf(1,"20180922000000",60);
@@ -178,6 +246,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户查询当前有效通话套餐
+     * @param userService
+     */
     private static void searchCurrentCallPackages(UserService userService){
         System.out.println("===========用户当前有效通话套餐===========");
         double t1 = System.currentTimeMillis();
@@ -189,6 +261,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户查询当前有效短信套餐
+     * @param userService
+     */
     private static void searchCurrentMessagePackages(UserService userService){
         System.out.println("===========用户当前有效短信套餐===========");
         double t1 = System.currentTimeMillis();
@@ -200,6 +276,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户查询当前有效本地流量套餐
+     * @param userService
+     */
     private static void searchCurrentLocalPackages(UserService userService){
         System.out.println("===========用户当前有效本地流量套餐===========");
         double t1 = System.currentTimeMillis();
@@ -211,6 +291,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户查询当前有效国内流量套餐
+     * @param userService
+     */
     private static void searchCurrentDomesticPackages(UserService userService){
         System.out.println("===========用户当前有效国内流量套餐===========");
         double t1 = System.currentTimeMillis();
@@ -222,6 +306,10 @@ public class HwApplication {
         System.out.println("使用时长："+(t2-t1)+"Millis");
     }
 
+    /**
+     * 用户查询本月通话记录
+     * @param userService
+     */
     private static void searchThisMonthCallRecord(UserService userService){
         System.out.println("===========用户本月通话记录===========");
         ArrayList<String> list = userService.searchRecords(1,1,"20180912000000");
@@ -230,6 +318,10 @@ public class HwApplication {
         }
     }
 
+    /**
+     * 用户查询本月账单
+     * @param userService
+     */
     private static void searchThisMonthCost(UserService userService){
         System.out.println("===========用户本月消费===========");
         double t1 = System.currentTimeMillis();
